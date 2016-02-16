8b75f3fb98d454c51093f50927de293d820e5c8d
Benjamin Charity <ben@benjamincharity.com>
2016-02-15 21:51:59 -0500

    generated full dist

--- angular-ratings.js
+++ angular-ratings.js
@@ -1,51 +1,41 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define("angular-ratings", [], factory);
-	else if(typeof exports === 'object')
-		exports["angular-ratings"] = factory();
-	else
-		root["angular-ratings"] = factory();
-})(this, function() {
-return /******/ (function(modules) { // webpackBootstrap
+/******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-/******/
+
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-/******/
+
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-/******/
+
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-/******/
+
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
+
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-/******/
+
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/
-/******/
+
+
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-/******/
+
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-/******/
+
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-/******/
+
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
@@ -55,26 +45,28 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
-	
+
 	var _angularRatingsDirective = __webpack_require__(1);
-	
-	angular.module('bc.AngularRatings', []).directive('bcAngularRatings', _angularRatingsDirective.AngularRatingsDirective);
+
+	angular.module('angular-ratings', []).directive('bpcAngularRatings', _angularRatingsDirective.AngularRatingsDirective);
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
-	
+
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
 	exports.AngularRatingsDirective = AngularRatingsDirective;
 	/* global require */
-	
+
+	var templateUrl = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./stars.html\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
 	function AngularRatingsDirective() {
 	    'ngInject';
-	
+
 	    var directive = {
 	        restrict: 'E',
 	        replace: true,
@@ -84,30 +76,30 @@ return /******/ (function(modules) { // webpackBootstrap
 	            ratings: '=?',
 	            readOnly: '=?'
 	        },
-	        templateUrl: __webpack_require__(2),
+	        templateUrl: templateUrl,
 	        link: linkFunction,
 	        controller: StarRatingController,
 	        controllerAs: 'vm'
 	    };
-	
+
 	    return directive;
-	
+
 	    /*
 	     * Link
 	     */
 	    function linkFunction($scope, $element) {
-	
+
 	        if ($scope.readOnly) {
 	            $element.addClass('stars--disabled');
 	        }
 	    }
-	
+
 	    /**
 	     * Controller
 	     */
 	    function StarRatingController($scope) {
 	        var _this = this;
-	
+
 	        // If no ratings were passed in, build a default array
 	        if (!this.ratings) {
 	            this.ratings = [{
@@ -122,22 +114,22 @@ return /******/ (function(modules) { // webpackBootstrap
 	                rating: 5
 	            }];
 	        }
-	
+
 	        // Initialize stars with the passed in rating
 	        updateStars(this.ratingValue);
-	
+
 	        // Watch for rating changes
 	        $scope.$watch('vm.ratingValue', function (newValue) {
 	            if (newValue) {
 	                updateStars.call(_this, newValue);
 	            }
 	        });
-	
+
 	        /**
 	         * Update rating stars
 	         */
 	        function updateStars(currentRating) {
-	
+
 	            this.ratings.forEach(function (rating) {
 	                if (rating.rating && rating.rating <= parseInt(currentRating, 10)) {
 	                    rating.checked = true;
@@ -149,19 +141,5 @@ return /******/ (function(modules) { // webpackBootstrap
 	    }
 	}
 
-/***/ },
-/* 2 */
-/***/ function(module, exports) {
-
-	var angular=window.angular,ngModule;
-	try {ngModule=angular.module(["bc.AngularRatingsTemplates?prefix=./src"])}
-	catch(e){ngModule=angular.module("bc.AngularRatingsTemplates?prefix=./src",[])}
-	var v1="<div class=\"bpc-stars\" data-ng-class=\"{ 'stars--disabled': vm.readOnly }\"> <div class=\"bpc-stars__star\" data-ng-repeat=\"rating in vm.ratings track by $index\" data-ng-click=\"vm.ratingValue = rating.rating\"> <span class=\"bpc-stars__star-icon\" data-ng-class=\"rating.checked ? 'ion-ios-star' : 'ion-ios-star-outline'\"></span> </div> </div>";
-	ngModule.run(["$templateCache",function(c){c.put("stars.html",v1)}]);
-	module.exports=v1;
-
 /***/ }
-/******/ ])
-});
-;
-//# sourceMappingURL=angular-ratings.js.map
+/******/ ]);
